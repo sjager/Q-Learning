343ace99fd8949bd7e7756d4b47853611fbceceb
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-02-11 19:46:31 -0600

    added empty loop finder form

---
+++ Q-Learning/LoopFinderForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q_Learning
+{
+    public partial class LoopFinderForm : Form
+    {
+        public LoopFinderForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
